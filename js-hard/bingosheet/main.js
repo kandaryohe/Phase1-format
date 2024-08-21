c2787b661eaf4a0334901d218f33d914039d35f6
kandaryohe <kan0527ryo@gmail.com>
2024-08-22 02:56:02 +0900

    temp

--- js-hard/bingosheet/main.js
+++ js-hard/bingosheet/main.js
@@ -7,4 +7,33 @@
 //Oの列には61～75
 //それぞれ更新したら毎回ランダムに表示
 
-const bingoView = document.getElementById("view");
+const bingoview = document.querySelector(".view");
+
+const item1 = document.createElement("td");
+item1.textContent = "B";
+view.appendChild(item1);
+
+const item2 = document.createElement("td");
+item2.textContent = "I";
+view.appendChild(item2);
+
+const item3 = document.createElement("td");
+item3.textContent = "N";
+view.appendChild(item3);
+
+const item4 = document.createElement("td");
+item4.textContent = "G";
+view.appendChild(item4);
+
+const item5 = document.createElement("td");
+item5.textContent = "O";
+view.appendChild(item5);
+
+//<tr>
+//<td>John</td>
+//<td>Doe</td>
+//</tr>
+//<tr>
+//<td>Jane</td>
+//<td>Doe</td>
+//</tr>
---
+++ jstest/main.js
@@ -0,0 +1,14 @@
+const list = document.querySelector(".list");
+
+const item = document.createElement("li");
+item.textContent = "りんご";
+list.appendChild(item);
+
+const item2 = document.createElement("li");
+item2.textContent = "みかん";
+list.appendChild(item2);
+
+const item3 = document.createElement("li");
+item3.textContent = "もも";
+list.appendChild(item3);
+コントロールｚ;
