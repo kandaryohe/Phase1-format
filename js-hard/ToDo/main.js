2730393e0e3e435289508f0079c175651dab62af
kandaryohe <kan0527ryo@gmail.com>
2024-08-21 21:13:40 +0900

    上級

--- js-hard/ToDo/main.js
+++ js-hard/ToDo/main.js
@@ -1,23 +1,32 @@
 //追加ボタンの取得
-//追加ボタンを押した時の関数の定義
-//inputタグを取得
-//入力値の取得
-//完了ボタンと共にタスク下に表示
-//<div></div>を作成
-//<div>入力値<button>完了<button></div>
-//HTML上のどこに表示するか
-//完了ボタンを押すとタスクから削除
-
 const addBtn = document.getElementsByClassName("add-btn")[0];
 
+//追加ボタンを押した時の関数の定義
+//inputタグを取得
 addBtn.addEventListener("click", function () {
   const inputTag = document.getElementById("add-area");
+
+  //入力値の取得
   const inputText = inputTag.value;
+
+  //<div></div>を作成
+  //<div></div>という枠組みを作成
   const newTag = document.createElement("div");
-  newTag.innerHTML = `${inputText}<button>完了 `;
+
+  //newTagの中には入力された値が入る
+  newTag.innerHTML = `${inputText}`;
+
+  //<div>入力値<button>完了<button></div>
+  //完了ボタンを、作成、取得する
+  const compBtn = document.createElement("button");
+  compBtn.textContent = "完了";
+
+  //完了ボタンと共にタスク下に表示
+  //HTML上のどこに表示するか
+  //完了ボタンを押すとタスクから削除
   const lists = document.getElementById("todo");
   lists.appendChild(newTag);
-  newTag.addEventListener("click", function () {
-    lists.removeChild(lists.lastElementChild);
+  compBtn.addEventListener("click", function () {
+    lists.removeChild(newTag);
   });
 });
---
+++ js-hard/bingosheet/main.js
@@ -0,0 +1,10 @@
+//5×6のマスを作る
+//一番上の段はBINGO、、真ん中はfree
+//Bの列には1～15
+//Iも列には16～30
+//Nの列には31～45＋真ん中はfree
+//Gの列には46～60
+//Oの列には61～75
+//それぞれ更新したら毎回ランダムに表示
+
+const bingoView = document.getElementById("view");
